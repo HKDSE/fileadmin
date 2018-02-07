73ea5327d787505790b35a5dff42da0d61b6de50
chanwc <schanwc@qq.com>
2018-02-07 07:41:58 +0000

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,6 +1,22 @@
-<?php  
-$no =$_GET["no"];if ($no == '')$no ='http://www.google.com';
-$url =$no;$homepage = file_get_contents( $url );
-    $homepage= str_replace('<a href="http', '<a href="?no=http', $homepage);    $homepage= str_replace('<a href="/', '<a href="?no='.$no.'/', $homepage);
+<?php  
 
-echo $homepage;?><form action="2.php" method="get"> no: <input type="text" name="no"><br><input type="submit"></form>
+
+$no =$_GET["no"];
+if ($no == '')
+$no ='http://www.google.com';
+
+$url =$no;
+$homepage = file_get_contents( $url );
+
+
+    $homepage= str_replace('<a href="http', '<a href="?no=http', $homepage);
+    $homepage= str_replace('<a href="/', '<a href="?no='.$no.'/', $homepage);
+
+
+
+echo $homepage;
+?>
+<form action="2.php" method="get">
+ no: <input type="text" name="no"><br>
+<input type="submit">
+</form>
